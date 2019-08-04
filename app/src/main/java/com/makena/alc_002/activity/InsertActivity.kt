d6179d6f655ea84df475c60a2245122879255bb1
“Makena” <makenaroes@gmail.com>
2019-08-04 12:13:20 +0300

    add firebase db saving

--- app/src/main/java/com/makena/alc_002/activity/InsertActivity.kt
+++ app/src/main/java/com/makena/alc_002/activity/InsertActivity.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.makena.alc_002.R
+import com.makena.alc_002.activity.models.Deal
 import kotlinx.android.synthetic.main.insert_content.*
 
 class InsertActivity: AppCompatActivity() {
@@ -22,7 +24,33 @@ class InsertActivity: AppCompatActivity() {
         _dbRefenrece = _firebaseDb!!.reference.child("traveldeals")
 
         setActionBar(toolbar)
+
+        btnSave.setOnClickListener{
+            saveDeal()
+            Snackbar.make(it, "Deal saved", Snackbar.LENGTH_SHORT)
+            clean()
+        }
     }
+
+    private fun clean() {
+
+        txtTitle.text.clear()
+        txtDescription.text.clear()
+        txtPrice.text.clear()
+
+    }
+
+    private fun saveDeal() {
+        val title: String = txtTitle.text.toString()
+        val description: String = txtDescription.text.toString()
+        val price: String = txtPrice.text.toString()
+
+        val deal = Deal(title,description,price)
+
+        _dbRefenrece!!.push().setValue(deal)
+
+    }
+
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
          super.onCreateOptionsMenu(menu)
